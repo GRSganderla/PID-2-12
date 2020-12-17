f7bd6a6be95e423252ca41a360fa65d4729541df
Guilherme Rodrigues <grodriguessganderla@gmail.com>
2020-12-17 19:56:36 -0300

    Commit 02: Algoritmo de Canny feito

---
+++ teste.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+lista = np.array([[1, 2], [1,3], [2, 1], [2,3], [3, 1], [3, 2]])
+lista2 = np.array([[3,3], [3,2], [3,1], [2,3], [2,2], [2,1],[1,3],[1,2],[1,1]])
+
+obj = [2,1]
+
+foda = []
+
+print(lista)
+lista.sort()
+print(lista)
